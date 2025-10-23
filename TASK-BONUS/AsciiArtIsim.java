64b65bd20e3c6b9f3dba375487ada24333a291ed
250541020 Sıla Ağgül <250541020@firat.edu.tr>
2025-10-23 08:33:36 -0700

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+        System.out.println(" SSS   III  L       A ");
+        System.out.println("S       I   L      A A");
+        System.out.println(" SSS    I   L     AAAAA");
+        System.out.println("    S   I   L     A   A");
+        System.out.println("SSSS   III  LLLLL A   A");
+    }
+}
