b4ee7db49bc0ea9998c307552b50d06f648bbc5e
250541020 Sıla Ağgül <250541020@firat.edu.tr>
2025-10-23 08:31:51 -0700

    Update ParaBozma.java

---
+++ TASK-BONUS/ParaBozma.java
@@ -0,0 +1,11 @@
+public class ParaBozma {
+    public static void main(String[] args) {
+        System.out.println("278 TL icin para bozdurma:");
+        System.out.println("100 TL: 2 adet");
+        System.out.println("50 TL: 1 adet");
+        System.out.println("20 TL: 1 adet");
+        System.out.println("5 TL: 1 adet");
+        System.out.println("2 TL: 1 adet");
+        System.out.println("1 TL: 1 adet");
+    }
+}
