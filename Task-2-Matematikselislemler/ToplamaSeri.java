47961373f4e446854b1a5391507e067816dce16f
250541020 Sıla Ağgül <250541020@firat.edu.tr>
2025-10-23 00:01:36 -0700

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,9 @@
+public class ToplamaSeri {
+    public static void main(String[] args) {
+        // 1'den 9'a kadar olan sayıların toplamını hesapla
+        int sonuc = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+
+        // Sonucu ekrana yazdır
+        System.out.println("1 + 2 + ... + 9 = " + sonuc);
+    }
+}
