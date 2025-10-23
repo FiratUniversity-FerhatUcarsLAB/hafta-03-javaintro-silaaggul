a229f81afa7e2691f6e5b5dfb5b87207ca10f70d
250541020 Sıla Ağgül <250541020@firat.edu.tr>
2025-10-23 00:26:09 -0700

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,19 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        // Bilgileri değişken olarak tanımlayalım
+        String ad = "Sila";
+        String soyad = "Aggul";
+        String ogrenciNo = "250541020";
+        String bolum = "Yazilim Muhendisligi";
+
+        // Güzel bir formatla ekrana yazdırma
+        System.out.println("*****************************");
+        System.out.println("*        ISIM KARTI         *");
+        System.out.println("*****************************");
+        System.out.println("Ad         : " + ad);
+        System.out.println("Soyad      : " + soyad);
+        System.out.println("Öğrenci No : " + ogrenciNo);
+        System.out.println("Bölüm      : " + bolum);
+        System.out.println("*****************************");
+    }
+}
