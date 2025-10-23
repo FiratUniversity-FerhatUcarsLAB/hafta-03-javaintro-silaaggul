e6b3d4b59f087a326fd7a82ce1a2e3354cbb3286
250541020 Sıla Ağgül <250541020@firat.edu.tr>
2025-10-22 23:18:18 -0700

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Java'ya Hos Geldiniz");
+    }
+}
